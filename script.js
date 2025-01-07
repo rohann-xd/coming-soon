cffdb64a8d65980dbef3b6328c96448e60984fc1
Rohan Sadhwani <rohansadhwani5@gmail.com>
2025-01-07 14:25:22 +0530

    Added modal for contact us

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ document.getElementById("email-form").addEventListener("submit", function (e) {
     alert("Please enter a valid email.");
   }
 });
+
+// Get the modal
+var modal = document.getElementById('contact-modal');
+
+// Get the link that opens the modal
+var link = document.getElementById('contact-link');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks the link, open the modal
+link.onclick = function(event) {
+  event.preventDefault();
+  modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
